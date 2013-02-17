64781c62d110ce416dc52d906c32368700e118bc
Siren Coop User <coop@sft.mx>
2013-02-17 08:29:25 +0100

    testing code now supports multiple multiple test series changed directory structure to be overall better wrote Makefile that auto-assembles, auto-runs test all files are capable of running without assembly assembly is only required for delivery (not even then) fixed bug in parser (python .isalpha() accepts characters such as 'ä')

---
+++ testing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#quick functions for printing colored text easily
+#call with no argument to get 'reset' colcode
+def colcode(col=""):
+	return "\x1b[" + str(col) + "m"
+
+def colprint(text, col):
+	print(colcode(col) + text + colcode())
+
+def redprint(text):
+	colprint(text, "1;31")
+
+def greenprint(text):
+	colprint(text, 32)
+
+def cyanprint(text):
+	colprint(text, 36)
+
+def testseries(name, testfun, testtostr, *tests):
+	success = 0
+	count = len(tests)
+	countstr = str(count)
+	countstrlen = len(countstr)
+
+	for i, test in enumerate(tests):
+		print(name + ": " + str(i).zfill(countstrlen) + "/" + countstr + ": " + testtostr(test))
+		if(testfun(test)):
+			success += 1
+		
+	message = name + ": " + str(success).zfill(countstrlen) + "/" + countstr + " successfull" 
+	if success == count:
+		greenprint(message)
+		return True
+	else:
+		redprint(message)
+		return False
