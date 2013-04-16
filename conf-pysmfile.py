c8b8c09acc2f3497f370369ca518c56bf3724bd3
Jonas Jelten <jonas.jelten@gmail.com>
2013-04-16 13:19:19 +0200

    config-py: file for sftmake python conf created

---
+++ conf-pysmfile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+# this file is, again, part of [sft]make
+#
+# licensed GPLv3 or later, no warranty, gschichten.
+#
+# as the sftmake config language and it's parsing is struggling
+# in it's development, this is an attempt to configure sftmake
+# via the regular python language
+#
+# (c) 2013 [sft]technologies
+
+
+
