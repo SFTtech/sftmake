d0dfb3b80f4d1828437d1b558e92bcbbe5fab261
Jonas Jelten <jonas.jelten@gmail.com>
2013-07-08 02:52:51 +0200

    tests: reenabled parser testing and disabled the builder test

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,10 @@ from logger.levels import *
 from logger.exceptions import handle_exceptions
 
 def run():
-	important("tests.builder.run()")
-	import tests.builder
+	#important("tests.builder.run()")
+	#import tests.builder
 
-	return #jj does not need the parser test yet..
+	#return
 
 	important("tests.parser.run()")
 	import tests.parser
