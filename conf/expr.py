154064f926a71e0d566be40bd729f590902294f6
Michael Ensslin <michael@ensslin.cc>
2013-07-08 14:57:24 +0200

    conf/expr.py: finished Expression subclasses
    the classes for representing expressions are now done and fully documented
    they correlate directly with the [literal, funccall, varsubst, expressionlist] nonterminals in the 'expresion' rule.
    there is also a 'concatenation' class, which correlates to the outer '*' in the 'expression' rule.

--- conf/expr.py
+++ conf/expr.py
@@ -1,5 +1,7 @@
 import conf.variable as variable
-import util
+import conf.functions as functions
+import conf.variables as variables
+import itertools
 
 class Expression:
 	def __init__(self, conf):
@@ -65,12 +67,34 @@ class FuncCall(Expression):
 		funcargs:
 			ExpressionList
 		"""
-		self.funcname = funcname
-		self.funcargs = funcargs
+		self.funcnameexpr = funcname
+		self.funcargsexpr = funcargs
 		super().__init__(conf)
 
 	def eval(self, conf, depends, valtype):
-		fname = self.funcname.eval(conf, depends, variable.VALTYPE_STRING)
+		funcname = self.funcnameexpr.eval(conf, depends, variable.VALTYPE_STRING)
+		func = functions[funcname]
+		args = self.funcargsexpr.eval(conf, depends, func.valtype)
+		ret = func.call(conf, args)
+		return self.typeconv(ret, valtype)
+
+class VarSubst(Expression):
+	"""
+	a substitution with values of an other variable
+	"""
+	def __init__(self, conf, varname):
+		"""
+		varname:
+			Expression
+		"""
+		self.varnameexpr = varname
+		super().__init__(conf)
+
+	def eval(self, conf, depends, valtype):
+		varname = self.varnamexpr.eval(conf, depends, variable.VALTYPE_STRING)
+		var = variables[varname]
+		ret = var.eval(conf, depends)
+		return self.typeconf(ret, valtype)
 
 class ExpressionList(Expression):
 	"""
@@ -88,154 +112,34 @@ class ExpressionList(Expression):
 		result = []
 		for expr in self.expressions:
 			result += expr.eval(conf, depends, valtype)
-		return result
 
-class ValTreeNode:
+class Concatenation(Expression):
 	"""
-	similar to CondTreeNode, represents one value tree node.
-	assignment values consist of a list of val tree nodes, which may themselves have more
-	value tree nodes as children.
-	consider the example: cflags += -g
-		the values are a single ValTreeNode of type Literal, with value '-g'.
-	consider the example: cflags += -g, -Wall, $(shell ls), $lflags, ${${mode}}
-		the values are four ValTreeNodes:
-		one literal, '-g'
-		one literal, '-Wall'
-		one function call, function name is a literal 'shell', function args is one literal 'ls'
-		one variable call, variable name is a literal 'lflags'
-		one variable call, variable name is a variable call, variable name is a literal 'mode'
+	a concatenation of multiple expressions
+	if some of the members evaluate to nothing, will evaluate to the empty list
+	if one of the members evaluate to multiple strings, will evaluate to multiple strings
+	if multiple of the members evaluate to multiple strings each, will evaluate to their cross product
+
+	the first member is evaluated with the normal valtype, all others as STRING.
 	"""
-
-	def __init__(self, conf):
+	def __init__(self, conf, expressions)
 		"""
-		conf
-			configuration where the ValTreeNode was declared
+		expressions:
+			list(Expression)
 		"""
-		self.conf = conf
-
-	def eval(self, evalconf, valtype, depends):
-		"""
-		evaluates this node and returns the result values as a list of strings
-
-		evalconf
-			configuration for which we are evaluating
-			needed e.g. when calling variables, or evaluating conditions
-		valtype
-			type that we expect the eval() to return (STRING, PATH, INT, ...)
-		depends
-			for detecting circular variable dependencies (and thus infinite loops)
-		"""
-		#not implemented in the abstract base class
-		raise NotImplementedError()
-
-
-class ValTreeNode_List(ValTreeNode):
-	"""
-	contains a list of ValTreeNodes
-	"""
-	def __init__(self, conf, nodes):
-		"""
-		nodes
-			list of ValTreeNodes
-		"""
+		self.expressions = expressions
 		super().__init__(conf)
-		self.nodes = nodes
 
-	def __repr__(self):
-		return repr(self.nodes)
+	def eval(self, conf, depends, valtype):
+		crossproduct = itertools.product((
+			expr.eval(
+				conf,
+				depends,
+				i == 0 and valtype or variable.VALTYPE_STRING
+			)
+			for i, expr in enumerate(self.expressions)
+		))
 
-	def eval(self, evalconf, valtype, depends):
-		result = []
-		for node in self.nodes:
-			result += node.eval(evalconf, valtype, depends)
+		result = ["".join(element) for element in crossproduct]
 
-		return self.typecheck(result, valtype)
-
-class ValTreeNode_StringLiteral(ValTreeNode):
-	"""
-	contains one string literal
-	"""
-	def __init__(self, conf, val):
-		"""
-		val
-			the literal value
-		"""
-		super().__init__(conf)
-		self.val = val
-
-	def __repr__(self):
-		return '"' + self.val + '"'
-
-	def eval(self, evalconf, valtype, depends):
-		return self.typecheck([val], valtype)
-
-class ValTreeNode_Var(ValTreeNode):
-	"""
-	evaluates an other variable, by its name
-	varname obviously is a ValTreeNode itself
-	"""
-	def __init__(self, conf, varname):
-		"""
-		varname
-			Variable name (must eval to exactly one string)
-		"""
-		super().__init__(conf)
-		self.varname = varname
-
-	def __repr__(self):
-		return '${' + repr(self.varname) + '}'
-
-	def eval(self, evalconf, valtype, depends):
-		#first, get the variable name
-		varname = self.varname.eval(evalconf, Var.VALTYPE_STRING, depends)
-		if len(varname) != 1:
-			#TODO some more sophisticated error reporting
-			raise Exception("varname must be exactly one string, but is " + repr(varname))
-		varname = varname[0]
-
-		#now, eval that variable
-		result = variables[varname[0]].eval(evalconf, depends)
-
-		return self.typecheck(result, valtype)
-
-class ValTreeNode_Fun(ValTreeNode):
-	"""
-	runs a function, by name and parameters
-	"""
-	def __init__(self, conf, funname, args):
-		"""
-		funname
-			Function name (must eval to exactly one string)
-		args
-			Function arguments
-			the valtype depends on the function name
-		"""
-		super().__init__(conf)
-		self.funname = funname
-		self.args = args
-
-	def __repr__(self):
-		return '$(' + repr(self.funname) + ' ' + repr(self.args) + ')'
-
-	def eval(self, evalconf, valtype, depends):
-		#first, get the function name
-		funname = self.funname.eval(evalconf, Var.VALTYPE_STRING, depends)
-		if len(funname) != 1:
-			raise Exception("funnae must be exactly one string, but is " + repr(funname))
-		funname = funname[0]
-
-		#TODO modular function concept, with user-definable functions etc etc
-		if funname == "count":
-			#the 'count' function requires its arguments to be of VALTYPE_STRING
-			args = self.args.eval(evalconf, Var.TYPE_STRING, depends)
-
-			#run the 'count' function magic (its really complicated!)
-			result = [str(len(args))]
-		elif funname == "path":
-			#the 'path' function has the only function to force the evaluation of its args as TYPE_PATH
-			args = self.args.eval(evalconf, Var.TYPE_PATH, depends)
-			result = args
-		else:
-			raise Exception("unknown function: " + funname)
-
-		return self.typecheck(result, valtype)
+		return self.typeconv(result, valtype)
