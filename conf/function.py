7577f70e8b33c66ff4c405be362370b5f84f38f2
Michael Ensslin <michael@ensslin.cc>
2013-07-08 14:56:44 +0200

    conf/function.py: completed documentation, functionality
    still unresolved: how does the user declare a function?

--- conf/function.py
+++ conf/function.py
@@ -3,7 +3,24 @@ from util.misc import inf
 functions = {}
 
 class Function:
+	"""
+	represents one in-config 'function', which can be used in config files
+	"""
 	def __init__(self, name, valtype, code, minargc = 0, maxargc = inf):
+		"""
+		name
+			name used to invoke the function in config files
+		valtype
+			the variable.valtype that the function is expecting
+		code
+			a python callable, which accepts the conf for which we are
+			evaluating as its first argument, followed by any amount
+			of string arguments
+		minargc
+			the minimum number of string arguments that the function requires
+		maxargc
+			the maximum number of string arguments that the function allows
+		"""
 		self.name = name
 		self.valtype = valtype
 		self.code = code
@@ -11,7 +28,10 @@ class Function:
 		self.maxargc = maxargc
 		functions[name] = self
 
-	def call(vallist):
+	def call(conf, vallist):
 		if len(vallist) < self.minargc:
-			raise 
-		return self.code(vallist)
+			raise FunctionTooFewArgumentsException(self.name, len(vallist), self.minargc)
+		if len(vallist) > self.maxargc:
+			raise FunctionTooMuchArgumentsException(self.name, len(vallist), self.maxargc)
+
+		return self.code(conf, *vallist)
